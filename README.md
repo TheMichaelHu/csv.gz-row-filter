02068f8b1fda42f475d4531c9e78fa18770d876e
Michael <michaelanhu@gmail.com>
2016-01-02 10:31:17 -0500

    actually works now, added real readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Ruby script that deletes rows in children csvs based on a regex. I'll add to this README later...
+# Filter for .csv.gz files
+
+## Overview
+
+A Ruby script that recursively finds all `.csv.gz` files in a given directory (also works with single files) and filters lines based on a hardcoded regex.
+
+I needed something like this for my [csvScatterplot](https://github.com/TheMichaelHu/csvScatterplot). I Googled for a good 20 seconds and couldn't find anything, so here we are.
+
+## Execution
+** Warning: Consider backing up data beforehand as this script is data destructive **
+
+Assign a regex to filter to `$FILTER_REGEX`
+Run `./filter [target file or dir]`
--- filter.rb
+++ filter.rb
@@ -3,23 +3,49 @@ require 'zlib'
 require 'fileutils'
 require 'tempfile'
 
-path = File.expand_path(ARGV[0])
-filter_regex = /^19.*/
+# Removes lines that don't match this regex anywhere
+$FILTER_REGEX = /^(?!19).*/
 
-Dir.glob(path+"**/*.csv.gz").each { |gz|
+def main
+	path = File.expand_path(ARGV[0])
+
+	if File.file?(path)
+		filter(path) if path =~ /\.csv.gz$/
+	elsif File.directory?(path)
+		Dir.glob(path+"/**/*.csv.gz").each { |gz|
+			filter(gz)
+		}
+	else
+		puts "#{path}: No such file or directory"
+	end
+end
+
+def filter(gz)
 	tmp = Tempfile.new('temp')
+	matched = false
+
 	Zlib::GzipReader.open(gz) { |csv|
 		csv.each_line { |line|
-			tmp.write(line) if (line =~ filter_regex).nil?
+			if line =~ $FILTER_REGEX
+				tmp.write(line)
+			else
+				matched = true
+			end
 		}
 	}
 	tmp.close
 
-	Zlib::GzipWriter.open(gz) { |filtered|
-	 File.readlines(tmp.path).each { |line|
-	  filtered.write(line)
-	 }
-	 filtered.close
-	}
+	if matched
+		Zlib::GzipWriter.open(gz) { |filtered|
+		 File.readlines(tmp.path).each { |line|
+		  filtered.write(line)
+		 }
+		 filtered.close
+		}
+		puts "Removed line(s) from #{gz}"
+	end
+
 	tmp.delete
-}
+end
+
+main
